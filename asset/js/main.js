5a1840716a27977e8c5d6bc4f075c7b058c3c6eb
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-03-30 17:03:29 +0200

    Esercizio completato + Bonus.

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,8 @@ var app = new Vue (
     {
         el :'#root',
         data :{
-            
+            greetings : 'Hello',
+            picture : './asset/img/javascript.png'
         },
     },
 );
